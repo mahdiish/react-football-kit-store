5f472413f2b88c40fd57c4b74acbbb34ee3cde95
mahdiish <mehdishayan1119@gmail.com>
2023-06-10 13:09:28 +0330

    SideBar modified to be sticky

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 import classNames from "classnames";
 
 const linkClassNames =
-  "mt-0 p-5 w-1/2 text-center text-white sm:mt-5 sm:w-full sm:text-left";
+  "mt-0 p-5 w-1/2 text-center sm:mt-5 sm:w-full sm:text-left";
 
 const activeLinkClassNames =
   "text-orange-500 font-bold bg-white border-b-4 border-l-0 border-orange-500 sm:border-l-4 sm:border-b-0";
@@ -10,12 +10,12 @@ const activeLinkClassNames =
 const classNamesFn = ({ isActive }) => {
   return isActive
     ? classNames(linkClassNames, activeLinkClassNames)
-    : classNames(linkClassNames);
+    : classNames(linkClassNames, "text-white");
 };
 
 function SideBar() {
   return (
-    <div className="flex flex-row h-16 w-full pt-0 bg-black border-white border-b sm:flex-col sm:w-1/6 sm:h-full sm:pt-5 sm:border-r">
+    <div className="flex flex-row h-16 w-full pt-0 bg-black border-white border-b sm:flex-col sm:w-1/5 sm:h-full sm:pt-5 sm:border-r sm:fixed sm:top-0 sm:left-0">
       <NavLink to="/" className={classNamesFn}>
         Store
       </NavLink>
